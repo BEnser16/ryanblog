f060cd4b4f3462553b1386f8161a43b090bf0163
BEnser16 <ryandai1690@gmail.com>
2024-01-20 04:30:37 +0800

    path handle

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
       <NavBar/>
       <Container>
         <Routes>
-          <Route path="/" element={<Home></Home>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/post" element={<Post/>} />
-          <Route path="/project" element={<Project/>} />
+          <Route path="/ryanblog/" element={<Home></Home>} />
+          <Route path="/ryanblog/about" element={<About/>} />
+          <Route path="/ryanblog/post" element={<Post/>} />
+          <Route path="/ryanblog/project" element={<Project/>} />
         </Routes>
       </Container>
       
